e7d4e39d423df8bd27c52087217a1cf18e999848
kaleginiene <ieva.kaleginiene@gmail.com>
2020-12-09 23:18:10 +0200

    Added some content into HomePage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,21 +2,35 @@ import React from "react";
 import { Hero, Button, Section } from "../../components";
 import * as S from "./Home.style";
 import Chef from "../../assets/logo.svg";
+import Food from "../../assets/content/food.jpg";
+import FoodBowl from "../../assets/content/bowl.jpg";
 
 function Home() {
   return (
     <S.Main>
       <Hero title="Anyone can COOK!" url={Chef} />
-      <Section background="e3e3e3">
-        Welcome to my React boilterplate. I hope that this will bring you up to
-        speed with your development.
+      <Section fullWidth background="eee">
+        <S.FlexBlock>
+          <S.HalfContainer>
+            <S.Image src={Food} className="picture" />
+          </S.HalfContainer>
+          <S.HalfContainer>
+            <S.H2>
+              Home of simple, delicous recipes from all over the world
+            </S.H2>
+            <S.Article>
+              In this website, you can find various recipes. Also, you can
+              create your account and make your own recipes list or upload your
+              recipe! Various recipes from cuisines all over the world:
+              desserts, breakfast, dinner or supper ideas are more than welcome.
+            </S.Article>
+            <S.Article>And remember, Anyone can COOK!</S.Article>
+            <S.Image src={FoodBowl} />
+          </S.HalfContainer>
+        </S.FlexBlock>
       </Section>
       <Section>
-        This boilerplate uses storybook, styled-components and React.
-      </Section>
-      <Section>
-        In this boilerplate you can find most kinds of inputs, styled loader and
-        themed style elements.
+        <h2>Check the recipes</h2>
       </Section>
       <Section>
         <Button color="primary">Check React Out</Button>
--- src/pages/Home/Home.style.js
+++ src/pages/Home/Home.style.js
@@ -3,3 +3,47 @@ import styled from "styled-components";
 export const Main = styled.main`
   min-height: 65vh;
 `;
+
+export const FlexBlock = styled.div`
+  width: 56em;
+  max-width: 100%;
+  margin: 0 auto;
+  display: flex;
+  @media only screen and (max-width: 691px) {
+    display: block;
+  }
+`;
+
+export const HalfContainer = styled.div`
+  width: calc(50% - 1.5em);
+  box-sizing: border-box;
+  padding: 4em 0 1.5em;
+  &:first-child {
+    margin-right: 5em;
+    @media only screen and (max-width: 691px) {
+      margin-right: 0;
+    }
+  }
+  @media only screen and (max-width: 691px) {
+    width: 100%;
+    padding-top: 1em;
+  }
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  &.picture {
+    @media only screen and (max-width: 691px) {
+      display: none;
+    }
+  }
+`;
+
+export const H2 = styled.h2`
+  margin-top: 0;
+`;
+
+export const Article = styled.article`
+  padding: 1em 0;
+  text-align: justify;
+`;
