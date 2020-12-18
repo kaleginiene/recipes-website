2732f791526a987f6312e6ba8429d586fd27ede5
kaleginiene <ieva.kaleginiene@gmail.com>
2020-12-19 00:20:26 +0200

    created recipe my-recipe router page

--- src/Routes.js
+++ src/Routes.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Header, Loader, Footer } from "./components";
 const Home = lazy(() => import("./pages/Home/Home"));
-const AddRecipe = lazy(() => import("./pages/AddRecipe/AddRecipe"));
 const Login = lazy(() => import("./pages/Auth/Login/Login"));
 const Register = lazy(() => import("./pages/Auth/Register/Register"));
+const AddRecipe = lazy(() => import("./pages/AddRecipe/AddRecipe"));
+const MyRecipes = lazy(() => import("./pages/MyRecipes/MyRecipes"));
+const Recipe = lazy(() => import("./pages/Recipe/Recipe"));
 
 function Routes() {
   return (
@@ -14,9 +16,11 @@ function Routes() {
       <Suspense fallback={<Loader />}>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/recipe" component={Recipe} />
           <Route exact path="/add-recipe" component={AddRecipe} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
+          <Route exact path="/my-recipes" component={MyRecipes} />
         </Switch>
       </Suspense>
       <Footer />
